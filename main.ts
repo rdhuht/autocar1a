78a5772e2caef8b43dab1f683b25e2742ca7bebd
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2022-01-17 14:31:53 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
-//  左侧电机
-function LMotor(Dir: boolean, Speed: number) {
+/**
+ * 初始化
+ */
+// 左侧电机
+function LMotor (Dir: boolean, Speed: number) {
     if (Dir == true) {
         Motor.MotorPulse(12, Speed)
         Motor.MotorPulse(13, 0)
@@ -7,11 +10,9 @@ function LMotor(Dir: boolean, Speed: number) {
         Motor.MotorPulse(12, 0)
         Motor.MotorPulse(13, Speed)
     }
-    
 }
-
-//  右侧电机
-function RMotor(Dir2: boolean, Speed2: number) {
+// 右侧电机
+function RMotor (Dir2: boolean, Speed2: number) {
     if (Dir2 == true) {
         Motor.MotorPulse(14, 0)
         Motor.MotorPulse(15, Speed2)
@@ -19,17 +20,14 @@ function RMotor(Dir2: boolean, Speed2: number) {
         Motor.MotorPulse(14, Speed2)
         Motor.MotorPulse(15, 0)
     }
-    
 }
-
-//  直行3
-function straight3() {
+// 直行3
+function straight3 () {
     LMotor(true, BasePWM * 4)
     RMotor(true, BasePWM * 4)
 }
-
-//  获取超声波雷达测量值  单位mm
-function Get_UltraSonic(): number {
+// 获取超声波雷达测量值  单位mm
+function Get_UltraSonic () {
     pins.digitalWritePin(DigitalPin.P14, 0)
     control.waitMicros(2)
     pins.digitalWritePin(DigitalPin.P14, 1)
@@ -37,13 +35,11 @@ function Get_UltraSonic(): number {
     pins.digitalWritePin(DigitalPin.P14, 0)
     return pins.pulseIn(DigitalPin.P15, PulseValue.High) * 0.172
 }
-
-function load_PIN(pin: string): number {
+function load_PIN (pin: string) {
     let A: number;
-    let B: number;
-    let C: number;
-    
-    PIN_A = 9
+let B: number;
+let C: number;
+PIN_A = 9
     PIN_B = 10
     PIN_C = 11
     if (pin == "X1") {
@@ -67,23 +63,19 @@ function load_PIN(pin: string): number {
         B = 0
         C = 4095
     }
-    
     Motor.MotorPulse(PIN_A, A)
     Motor.MotorPulse(PIN_B, B)
     Motor.MotorPulse(PIN_C, C)
     basic.pause(18)
     return pins.analogReadPin(AnalogPin.P1)
 }
-
-//  右侧LED
-function RLED(Red: number, Green: number, Blue: number) {
+// 右侧LED
+function RLED (Red: number, Green: number, Blue: number) {
     Motor.MotorPulse(0, Red)
     Motor.MotorPulse(1, Green)
     Motor.MotorPulse(2, Blue)
 }
-
-function ana_to_sig() {
-    
+function ana_to_sig () {
     sl2 = false
     sr2 = false
     smid2 = false
@@ -91,55 +83,44 @@ function ana_to_sig() {
     if (SL > gap) {
         sl2 = true
     }
-    
     if (SR > gap) {
         sr2 = true
     }
-    
     if (SMID > gap) {
         smid2 = true
     }
-    
 }
-
-//  右转
-function turn_Right() {
+// 右转
+function turn_Right () {
     LMotor(true, BasePWM * 2)
     RMotor(false, BasePWM * 2)
 }
-
-//  左转
-function turn_Left() {
+// 左转
+function turn_Left () {
     LMotor(false, BasePWM * 2)
     RMotor(true, BasePWM * 2)
 }
-
-//  刹车
-function brake() {
+// 刹车
+function brake () {
     LMotor(true, 0)
     RMotor(true, 0)
 }
-
-//  直行1
-function straight1() {
+// 直行1
+function straight1 () {
     LMotor(true, BasePWM)
     RMotor(true, BasePWM)
 }
-
-//  直行2
-function straight2() {
+// 直行2
+function straight2 () {
     LMotor(true, BasePWM * 2)
     RMotor(true, BasePWM * 2)
 }
-
-//  左侧LED
-function LLED(Red2: number, Green2: number, Blue2: number) {
+// 左侧LED
+function LLED (Red2: number, Green2: number, Blue2: number) {
     Motor.MotorPulse(6, Red2)
     Motor.MotorPulse(7, Green2)
     Motor.MotorPulse(8, Blue2)
 }
-
-/** 初始化 */
 let ThreeBlackCounter = 0
 let Distance = 0
 let SMID = 0
@@ -155,35 +136,36 @@ let PIN_A = 0
 let BasePWM = 0
 BasePWM = 900
 serial.redirectToUSB()
-basic.forever(function on_forever() {
-    
-    //  basic.show_string("Beihang",100)
+basic.forever(function () {
+    // basic.show_string("Beihang",100)
     SL = load_PIN("X3")
     SMID = load_PIN("X2")
     SR = load_PIN("X1")
     Distance = Get_UltraSonic()
-    //  Distance=0
-    serial.writeLine("" + ("" + SL) + "," + ("" + ("" + SMID)) + "," + ("" + ("" + SR)) + "," + ("" + ("" + Distance)) + "," + ("" + ("" + ThreeBlackCounter)))
-    //  模拟量转信号量
+    // Distance=0
+    serial.writeLine("" + SL + "," + ("" + SMID) + "," + ("" + SR) + "," + ("" + Distance) + "," + ("" + ThreeBlackCounter))
+    // 模拟量转信号量
     ana_to_sig()
-    //  判断动作
+    // 判断动作
     if (Distance < 50) {
         brake()
+        LLED(1023, 0, 0)
+        RLED(1023, 0, 0)
     } else if (sl2 == true && sr2 == false) {
         turn_Left()
-        LLED(1023, 1023, 1023)
+        LLED(0, 1023, 0)
         RLED(0, 0, 0)
         ThreeBlackCounter = 0
     } else if (sl2 == false && sr2 == true) {
         turn_Right()
-        RLED(1023, 1023, 1023)
+        RLED(0, 1023, 0)
         LLED(0, 0, 0)
         ThreeBlackCounter = 0
     } else if (sl2 == false && sr2 == false) {
         if (smid2 == false) {
             straight1()
             basic.showIcon(IconNames.Heart, 60)
-            LLED(1023, 1023, 1023)
+LLED(1023, 1023, 1023)
             RLED(1023, 1023, 1023)
             ThreeBlackCounter = 0
         } else {
@@ -192,7 +174,6 @@ basic.forever(function on_forever() {
             RLED(2047, 2047, 2047)
             ThreeBlackCounter = 0
         }
-        
     } else if (sl2 == true && sr2 == true && smid2 == true) {
         ThreeBlackCounter = ThreeBlackCounter + 1
         if (ThreeBlackCounter < 5) {
@@ -204,13 +185,11 @@ basic.forever(function on_forever() {
             LLED(0, 0, 0)
             brake()
         }
-        
     } else {
-        //  straight3();
+        // straight3();
         straight1()
         LLED(1023, 1023, 1023)
         RLED(1023, 1023, 1023)
         ThreeBlackCounter = 0
     }
-    
 })
